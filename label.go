0edc8f8cd96134d866fe8e90fcf7da528d6a46e5
Naiba <1@5.nu>
2019-02-20 13:43:09 +0800

    :paperclip: Add label

---
+++ label.go
@@ -0,0 +1,14 @@
+package solitudes
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Label 标签
+type Label struct {
+	gorm.Model
+	Name string
+	Slug string `gorm:"unique_index"`
+
+	Articles []Article
+}
