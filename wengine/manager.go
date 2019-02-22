73e7a2ed6a1d2fbd1b5805ef19032c84e4dc2a5c
Naiba <1@5.nu>
2019-02-22 11:00:59 +0800

    :sunglasses: Dashboard index

--- wengine/manager.go
+++ wengine/manager.go
@@ -2,6 +2,10 @@ package wengine
 
 import (
 	"net/http"
+	"runtime"
+	"time"
+
+	"github.com/naiba/solitudes"
 
 	"github.com/naiba/solitudes/x/soligin"
 
@@ -9,5 +13,31 @@ import (
 )
 
 func manager(c *gin.Context) {
-	c.HTML(http.StatusOK, "admin/index", soligin.Soli(gin.H{}))
+	var articleNum, commentNum, labelNum int
+	var lastArticle solitudes.Article
+	var lastComment solitudes.Comment
+	solitudes.System.D.Model(solitudes.Article{}).Count(&articleNum)
+	solitudes.System.D.Model(solitudes.Comment{}).Count(&commentNum)
+	solitudes.System.D.Model(solitudes.Label{}).Count(&labelNum)
+	solitudes.System.D.Select("created_at").Order("id DESC").First(&lastArticle)
+	solitudes.System.D.Select("created_at").Order("id DESC").First(&lastComment)
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	c.HTML(http.StatusOK, "admin/index", soligin.Soli(gin.H{
+		"articleNum":         articleNum,
+		"commentNum":         commentNum,
+		"lastArticlePublish": time.Now().Sub(lastArticle.CreatedAt).Hours() / 24,
+		"lastComment":        time.Now().Sub(lastComment.CreatedAt).Hours() / 24,
+		"labelNum":           labelNum,
+
+		"memoryUsage": bToMb(m.Alloc),
+		"gcNum":       m.NumGC,
+		"routineNum":  runtime.NumGoroutine(),
+	}))
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
