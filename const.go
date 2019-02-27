a49f0cd55deca8176bb4032eff11efd7e4cfb8ac
Naiba <1@5.nu>
2019-02-27 10:04:45 +0800

    :smile: i like solitude

--- const.go
+++ const.go
@@ -13,12 +13,12 @@ import (
 const (
 	// CtxAuthorized 用户已认证
 	CtxAuthorized = "cazed"
-	// AuthCookie 用户认证使用的Cookie名
-	AuthCookie = "i_am_solitude"
 	// CtxPassPreHandler 通过了PreHandler
 	CtxPassPreHandler = "cpph"
 	// CtxRequestParams 路由参数
 	CtxRequestParams = "crp"
+	// AuthCookie 用户认证使用的Cookie名
+	AuthCookie = "i_like_solitude"
 )
 
 // SysVeriable 全局变量
